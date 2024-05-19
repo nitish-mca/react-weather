39c4e8cdbf086ab8014e3e5aa2427d1ed42e00cf
Nitish Kumar <nitish.kumar1820@gmail.com>
2024-05-19 22:57:17 +0530

    Adding api;

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Getting Started with Create React App
 
+This project will fetch weather for any city. it is using openweathermap api to get data
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const api = {
+  key: "4ef4a58c2d6324fa309907d9e8ff0d56",
+  baseUrl: "https://api.openweathermap.org/data/2.5"
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt =>{
+    console.log(query)
+    if(evt.key ==="Enter"){
+      fetch(`${api.baseUrl}/weather?q=${query}&units=Metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+      })
+    }
+  }
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(weather.main && weather.main.temp) ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search Your City..."
+            className="search-bar"
+            onChange={e=> setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(weather.name) ? (
+        <div>
+          <div className="location-box">
+            <div className="location"> {weather.name}, {weather['sys'].country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div><div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ):(<div>Data not found</div>)}
+
+      </main>
     </div>
   );
 }
